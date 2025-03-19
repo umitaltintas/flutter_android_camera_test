d15e844619fb70fdd596373d7aad05025ad31693
Ümit Altıntaş <57873472+umitaltintas@users.noreply.github.com>
2025-03-19 16:03:12 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/camera_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/camera_test/MainActivity.kt
@@ -4,21 +4,21 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
-    private var camera2Handler: Camera2Handler? = null
-    
+    private var cameraXHandler: CameraXHandler? = null
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        
-        // Initialize Camera2 handler
-        camera2Handler = Camera2Handler(
+
+        // Initialize CameraX handler
+        cameraXHandler = CameraXHandler(
             this,
             flutterEngine.dartExecutor.binaryMessenger,
             flutterEngine.renderer
         )
     }
-    
+
     override fun onDestroy() {
-        camera2Handler?.dispose()
+        cameraXHandler?.dispose()
         super.onDestroy()
     }
 }
